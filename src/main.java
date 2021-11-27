18e0a583f1a41cc805fb5fab7a1ed3cb7753c3f4
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2021-11-27 16:21:40 +0300

    miles

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        long ticketPrice = 500;
+        long rub = 20;
+        long bonus = ticketPrice / rub;
+        System.out.println("Здравствуйте! Благодарим вас за выбор нашей авиакомпании для вашего рейса. Ваш бонус составляет:"+bonus+" миль");
+
+    }
+}
