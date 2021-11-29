5d9e104b17fb501bd734907ee1e0e135ab706975
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2021-11-29 22:47:44 +0300

    изменение слов

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+        public static void main(String[] args) {
+            long ticketPrice = 500;
+            long rub = 20;
+            long bonus = ticketPrice / rub;
+            System.out.println("Здравствуйте! Благодарим вас за выбор нашей авиакомпании для вашего рейса. Ваш бонус составляет:"+bonus+" миль");
+
+        }
+
+}
